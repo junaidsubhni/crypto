c783f23bc06848af5c91b04f97ff2db4ea49b5c3
junaid.subhani <junaid.subhani@callsign.com>
2022-04-04 04:28:00 +0400

    Initial commit project settup

---
+++ src/test/java/com/zooplus/crypto/CryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooplus.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
